066389efef1185dbc14ef334c54dada64bd3a7bd
Andrey <aa89popov@mail.ru>
2025-07-23 00:55:44 +0300

    All functions are added to the class

--- cat.py
+++ cat.py
@@ -1,98 +1,99 @@
 import json
 import requests
-import time
+from time import sleep
+from tqdm import tqdm
 
-def create_folder(token):
-    """Creates a "PD-132" folder on Yandex.Disk"""
-    url = 'https://cloud-api.yandex.net/v1/disk/resources'
-    headers = {
-        'Authorization': f'OAuth {token}'
-    }
-    params = {
-        'path': 'PD-132'
-    }
-    response = requests.put(url, headers=headers, params=params)
-    if response.status_code in [201, 409]:
-        return response.status_code
-    else:
-        print(f"Произошла ошибка при создании папки: {response.text}")
-        return None
 
-def get_url_image(text_on_image):
-    """gets a link to download an image using an API request"""
-    if text_on_image == "":
-        text_on_image = "Hello"
-    url = 'https://cataas.com/cat/says/'
-    params = {
-        'json': True
-    }
-    response = requests.get(url + text_on_image, params=params)
-    if response.status_code == 200:
-        return response.json()['url']
-    else:
-        print(f"Произошла ошибка при получении картинки: {response.text}")
-        return None
+class YandexDiscAPI:
 
-def upload_image_on_ydisk(token, text_on_image):
-    """
-    Uploads a picture to Yandex.Disk.
-    Returns the ID of this operation.
-    """
-    url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-    headers = {
-        'Authorization': f'OAuth {token}'
-    }
-    params = {
-        'url': f'{get_url_image(text_on_image)}',
-        'path': f'PD-132/{text_on_image}'
-    }
-    response = requests.post(url, headers=headers, params=params)
-    if response.status_code == 202:
-        operations_id = (response.json()['href'].split('/')[-1])
-        return operations_id
-    else:
-        print(f'Ошибка загрузки файла {response.text}')
-        return None
+    def __init__(self, token):
+        self.headers = {
+            'Authorization': f'OAuth {token}'
+        }
 
-def get_operation_status(token, operations_id):
-    """Checks the file upload status"""
-    url = "https://cloud-api.yandex.net/v1/disk/operations/"
-    headers = {
-        'Authorization': f'OAuth {token}'
-    }
-    count = 3
-    while count > 0:
-        response = requests.get(url + operations_id, headers=headers)
-        if response.json()['status'] == 'in-progress':
-            print(f'Производится загрузка файла {"..."*(4 - count)}')
-            time.sleep(3)
-            count -= 1
-        elif response.json()['status'] == 'success':
-            return print(f'Файл загружен успешно!')
+    def create_folder(self):
+        """Creates folder on Yandex.Disk"""
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        params = {
+            'path': 'PD-132'
+        }
+        response = requests.put(url, headers=self.headers, params=params)
+        if response.status_code in [201, 409]:
+            return response.status_code
         else:
-            return print(f'Ошибка загрузки файла {response.text}')
-    else:
-        return print(f'Превышено время ожидания загрузки файла')
+            print(f'Произошла ошибка при создании папки: {response.text}')
+            return None
 
-def get_json_file(token):
-    """Saves file information (name, size) from Yandex.Disk to a json file"""
-    url = 'https://cloud-api.yandex.net/v1/disk/resources'
-    headers = {
-        'Authorization': f'OAuth {token}'
-    }
-    params = {
-        'path': 'PD-132',
-        'fields': '_embedded.items.name, _embedded.items.size'
-    }
-    response = requests.get(url, headers=headers, params=params)
-    content_on_folder = response.json()['_embedded']['items']
-    with open('images_info.json', 'w', encoding='utf-8') as f:
-        json.dump(content_on_folder, f,indent=2)
-    return print(f'Информация сохранена в images_info.json')
+    def get_url_image(self, text_on_image='Hello'):
+        """
+        Gets a link to download an image using an API request
+        (default "Hello")
+        """
+        url = 'https://cataas.com/cat/says/'
+        params = {
+            'json': True
+        }
+        response = requests.get(url + text_on_image, params=params)
+        if response.status_code == 200:
+            return response.json()['url']
+        else:
+            print(f'Произошла ошибка при получении картинки: {response.text}')
+            return None
 
+    def upload_image_on_ydisk(self, name_image='Hello'):
+        """Uploads a picture to Yandex.Disk."""
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        params = {
+            'url': f'{self.get_url_image(name_image)}',
+            'path': f'PD-132/{name_image}'
+        }
+        response = requests.post(url, headers=self.headers, params=params)
+        if response.status_code == 202:
+            operations_id = (response.json()['href'].split('/')[-1])
+            for load_file in tqdm(range(1)):
+                self.get_operation_status(operations_id)
+            return
+        else:
+            print(f'Ошибка загрузки файла {response.text}')
+            return None
+
+    def get_operation_status(self, operations_id):
+        """Checks the file upload status"""
+        url = "https://cloud-api.yandex.net/v1/disk/operations/"
+        count = 3
+        while count > 0:
+            response = requests.get(url + operations_id, headers=self.headers)
+            if response.json()['status'] == 'in-progress':
+                sleep(3)
+                count -= 1
+            elif response.json()['status'] == 'success':
+                print(f'Файл загружен успешно!')
+                return
+            else:
+                print(f'Ошибка загрузки файла {response.text}')
+                return None
+        else:
+            print(f'Превышено время ожидания загрузки файла')
+            return None
+
+    def get_json_file(self):
+        """Saves file information (name, size) from Yandex.Disk to a json file"""
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        params = {
+            'path': 'PD-132',
+            'fields': '_embedded.items.name, _embedded.items.size'
+        }
+        response = requests.get(url, headers=self.headers, params=params)
+        content_on_folder = response.json()['_embedded']['items']
+        with open('images_info.json', 'w', encoding='utf-8') as f:
+            json.dump(content_on_folder, f, indent=2, ensure_ascii=False)
+        print(f'Информация сохранена в images_info.json')
+        return
+
+
+text = input('Введите текст для картинки: ')
 token = input("Введите токен Яндекс.Диска: ")
-text_on_image = input('Введите текст для картинки: ')
-create_folder(token)
-operations_id = upload_image_on_ydisk(token, text_on_image)
-get_operation_status(token, operations_id)
-get_json_file(token)
+yd_api = YandexDiscAPI(token)
+yd_api.create_folder()
+yd_api.upload_image_on_ydisk(text)
+yd_api.get_json_file()
